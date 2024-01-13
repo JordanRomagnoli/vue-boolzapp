feac4bd031702fab3e5a9f7b51f6b7866f50e3e2
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2024-01-13 15:11:27 +0100

    - FIXED funzionamento del filtraggio tramite dell'input

--- js/scripts.js
+++ js/scripts.js
@@ -8,21 +8,23 @@ createApp({
                     name: 'Michele',
                     avatar: 'img/avatar_1.jpg',
                     visible: false,
-                    dropCounter: false,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Hai portato a spasso il cane?',
+                            dropCounter: false,
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Ricordati di stendere i panni',
+                            dropCounter: false,
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 16:15:22',
                             message: 'Tutto fatto!',
+                            dropCounter: false,
                             status: 'received'
                         }
                     ],
@@ -31,21 +33,23 @@ createApp({
                     name: 'Fabio',
                     avatar: 'img/avatar_2.jpg',
                     visible: true,
-                    dropCounter: false,
                     messages: [
                         {
                             date: '20/03/2020 16:30:00',
                             message: 'Ciao come stai?',
+                            dropCounter: false,
                             status: 'sent'
                         },
                         {
                             date: '20/03/2020 16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
+                            dropCounter: false,
                             status: 'received'
                         },
                         {
                             date: '20/03/2020 16:35:00',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            dropCounter: false,
                             status: 'sent'
                         }
                     ],
@@ -54,21 +58,23 @@ createApp({
                     name: 'Samuele',
                     avatar: 'img/avatar_3.jpg',
                     visible: true,
-                    dropCounter: false,
                     messages: [
                         {
                             date: '28/03/2020 10:10:40',
                             message: 'La Marianna va in campagna',
+                            dropCounter: false,
                             status: 'received'
                         },
                         {
                             date: '28/03/2020 10:20:10',
                             message: 'Sicuro di non aver sbagliato chat?',
+                            dropCounter: false,
                             status: 'sent'
                         },
                         {
                             date: '28/03/2020 16:15:22',
                             message: 'Ah scusa!',
+                            dropCounter: false,
                             status: 'received'
                         }
                     ],
@@ -77,16 +83,17 @@ createApp({
                     name: 'Alessandro B.',
                     avatar: 'img/avatar_4.jpg',
                     visible: true,
-                    dropCounter: false,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
+                            dropCounter: false,
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Si, ma preferirei andare al cinema',
+                            dropCounter: false,
                             status: 'received'
                         }
                     ],
@@ -95,16 +102,17 @@ createApp({
                     name: 'Alessandro L.',
                     avatar: 'img/avatar_5.jpg',
                     visible: true,
-                    dropCounter: false,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ricordati di chiamare la nonna',
+                            dropCounter: false,
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Va bene, stasera la sento',
+                            dropCounter: false,
                             status: 'received'
                         }
                     ],
@@ -113,21 +121,23 @@ createApp({
                     name: 'Claudia',
                     avatar: 'img/avatar_6.jpg',
                     visible: true,
-                    dropCounter: false,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao Claudia, hai novità?',
+                            dropCounter: false,
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Non ancora',
+                            dropCounter: false,
                             status: 'received'
                         },
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'Nessuna nuova, buona nuova',
+                            dropCounter: false,
                             status: 'sent'
                         }
                     ],
@@ -136,16 +146,17 @@ createApp({
                     name: 'Federico',
                     avatar: 'img/avatar_7.jpg',
                     visible: true,
-                    dropCounter: false,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                            dropCounter: false,
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                            dropCounter: false,
                             status: 'received'
                         }
                     ],
@@ -154,21 +165,23 @@ createApp({
                     name: 'Davide',
                     avatar: 'img/avatar_8.jpg',
                     visible: true,
-                    dropCounter: false,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                            dropCounter: false,
                             status: 'received'
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                            dropCounter: false,
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'OK!!',
+                            dropCounter: false,
                             status: 'received'
                         }
                     ],
@@ -182,9 +195,8 @@ createApp({
     methods: {
 
         messageStatus(i) {
-            
-            this.contatore = i
 
+            this.contatore = i;
         },
 
         addMyMessage(){
@@ -217,24 +229,29 @@ createApp({
             
         },
 
-        filteredContacts() {
+        updateVisibility() {
             const lowerCaseSearch = this.searchChat.toLowerCase();
+          
+            for (let i = 0; i < this.contacts.length; i++) {
 
-            return this.contacts.filter((singleChat) => {
-              const lowerCaseName = singleChat.name.toLowerCase();
-
-              return lowerCaseName.includes(lowerCaseSearch);
-            });
+              const lowerCaseName = this.contacts[i].name.toLowerCase();
+              this.contacts[i].visible = lowerCaseName.includes(lowerCaseSearch);
+              
+            }
         },
 
         toggleDrop(i){
             
-            this.contacts[i].dropCounter = !this.contacts[i].dropCounter
+            this.contacts[this.contatore].messages[i].dropCounter = !this.contacts[this.contatore].messages[i].dropCounter
            
         },
 
-        removeMessage(array, i) {
-            array.splice(i, 1);
+
+        // ??
+        removeMessage(i) {
+
+            this.contacts[this.contatore].messages.splice(i, 1);
+
         },
 
     }
